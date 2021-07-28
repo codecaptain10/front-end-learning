11ec82ad801afbd027957e93e59b1d789d109537
Szymon Szaminko <sz.szaminko@gmail.com>
2021-07-28 12:55:43 +0200

    Update javascript file with variables and events

--- flexbox/app.js
+++ flexbox/app.js
@@ -1,19 +1,20 @@
-//global variables
+//GLOBAL VARIABLES
+//score
 let score = document.querySelector(".score");
-
+//numbers
 let numbers = document.querySelectorAll('.number');
 console.log(numbers);
-
-let add = document.querySelector(".add");
-let sub = document.querySelector(".sub");
-let multi = document.querySelector(".multi");
-let divide = document.querySelector(".divide");
-let dott = document.querySelector(".dott");
-let eq = document.querySelector(".eq");
-
-
-//Events
+//operations
+let operations = document.querySelectorAll('.operation');
+console.log(operations);
+//reset
+let reset = document.querySelector('.reset');
+//result
 let result = "";
+
+
+//EVENTS
+//Event for all numbers
 for (let i = 0; i < numbers.length; i++) {
     numbers[i].addEventListener('click', function() {
         score.textContent = result + numbers[i].textContent;
@@ -21,3 +22,17 @@ for (let i = 0; i < numbers.length; i++) {
         console.log(result);
     });
 }
+//Event for all operations
+for (let i = 0; i < operations.length; i++) {
+    operations[i].addEventListener('click', function() {
+        score.textContent = result + " " + operations[i].textContent + " ";
+        result = score.textContent;
+        console.log(result);
+    });
+}
+//Event for reset 
+reset.addEventListener('click', function() {
+    score.textContent = "";
+    result = score.textContent;
+    console.log(result);
+});
