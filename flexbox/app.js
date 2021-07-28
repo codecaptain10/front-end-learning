de081b9ec79b9174c8738938c00bf395768b22fd
Szymon Szaminko <sz.szaminko@gmail.com>
2021-07-28 12:46:45 +0200

    Update javascript file with variables

--- flexbox/app.js
+++ flexbox/app.js
@@ -1,16 +1,8 @@
 //global variables
 let score = document.querySelector(".score");
 
-let num0 = document.querySelector('.num0');
-let num1 = document.querySelector('.num1');
-let num2 = document.querySelector('.num2');
-let num3 = document.querySelector('.num3');
-let num4 = document.querySelector('.num4');
-let num5 = document.querySelector('.num5');
-let num6 = document.querySelector('.num6');
-let num7 = document.querySelector('.num7');
-let num8 = document.querySelector('.num8');
-let num9 = document.querySelector('.num9');
+let numbers = document.querySelectorAll('.number');
+console.log(numbers);
 
 let add = document.querySelector(".add");
 let sub = document.querySelector(".sub");
@@ -22,10 +14,10 @@ let eq = document.querySelector(".eq");
 
 //Events
 let result = "";
-
-
-num1.addEventListener('click', function() {
-    score.textContent = result + "1";
-    result = score.textContent;
-    console.log(result)
-})
+for (let i = 0; i < numbers.length; i++) {
+    numbers[i].addEventListener('click', function() {
+        score.textContent = result + numbers[i].textContent;
+        result = score.textContent;
+        console.log(result);
+    });
+}
