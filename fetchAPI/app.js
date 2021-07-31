22c8ab183d39bf7a143caadb23018a8c3d8a34cc
Szymon Szaminko <sz.szaminko@gmail.com>
2021-07-31 03:15:10 +0200

    First look for fetch method in my project

--- fetchAPI/app.js
+++ fetchAPI/app.js
@@ -1,12 +1,23 @@
 //-----VARIABLES-----
 
+//const { response } = require("express");
+
 //url adress
 const url = 'https://randomuser.me/api/?results=10';
 //Function getUsers
 const getUsers = () => {
     //fetch method from window object
     fetch(url)
-        .then(response => console.log(response))
+        .then(response => {
+            // console.log(response);
+            if (response.status !== 200) {
+                throw Error("To nie jest odpowiedź 200");
+            } else {
+                //JSON to JavaScript Object
+                return response.json();
+            }
+        })
+        .then(json => console.log(json))
         .catch(err => console.log(err))
 }
 
