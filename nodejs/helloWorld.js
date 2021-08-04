d9543591940d8bd4637a9f0d60aad51db477cd59
Szymon Szaminko <sz.szaminko@gmail.com>
2021-08-04 12:49:11 +0200

    Create helloWorld.js file and first code in Node.js

---
+++ nodejs/helloWorld.js
@@ -0,0 +1,12 @@
+//First code in Node.js
+/*
+  1. Create a variable
+  2. Add the variable to a console.log
+  3. Open CLI
+  4. Set the right path to your file 
+  5. In CLI write "node helloWorld.js"
+  6. "Hello Wordl!"
+  7. In CLI write "clear" to clear console
+*/
+const sayHello = "Hello World!";
+console.log(sayHello);
