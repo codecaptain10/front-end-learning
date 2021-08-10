2829bb1e771dd78f8720e0bc2339226225a4fcd5
Szymon Szaminko <sz.szaminko@gmail.com>
2021-08-10 14:52:29 +0200

    Create a order and file to npm example with colors.js package

---
+++ nodejs/npm-example/app.js
@@ -0,0 +1,25 @@
+ /* NPM Example
+           - Node Package Manager
+           - To download and management of packages (modules) 
+
+           -Example: color.js package
+             Source : https://www.npmjs.com/package/colors
+             How to use? : All needed information we have in docs
+            
+                
+           */
+
+ //Import
+ const colors = require('colors');
+
+ //Some examples from docs
+ //  console.log('hello'.green); // outputs green text
+ //  console.log('i like cake and pies'.underline.red) // outputs red underlined text
+ //  console.log('inverse the color'.inverse); // inverses the color
+ //  console.log('OMG Rainbows!'.rainbow); // rainbow
+ //  console.log('Run the trap'.trap); // Drops the bass
+
+ //Hello World in rainbow
+ console.log('---------------'.rainbow);
+ console.log('| Hello World! |'.rainbow);
+ console.log('---------------'.rainbow);
