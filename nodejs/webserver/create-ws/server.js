4df7c9dd5260f6fb40ff8dae364b906a482a5de4
Szymon Szaminko <sz.szaminko@gmail.com>
2021-08-14 11:48:10 +0200

    Create example web server with basic commands

---
+++ nodejs/webserver/create-ws/server.js
@@ -0,0 +1,10 @@
+/*This is a file to instruct the basic about creating web server in Node.js */
+
+
+const http = require('http');
+http.createServer((request, response) => {
+    response.writeHead(200, {
+        'Content-Type': 'text/html'
+    });
+    response.end('<h1> Hello Node.js! I am your web server!</h1>')
+}).listen(3000, '127.0.0.1');
