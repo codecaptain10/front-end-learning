1c4d786d54a768f9e1f2e2d165317d4ed5a312c5
Szymon Szaminko <sz.szaminko@gmail.com>
2021-08-15 23:19:15 +0200

    Create and modife example about routing in Node.js web server and simple application

--- nodejs/webserver-examples/ws-ex4-routing/server.js
+++ nodejs/webserver-examples/ws-ex4-routing/server.js
@@ -3,11 +3,46 @@
   - Serwer potrafi zwrocic inne zasoby w zaleznosci jaka sciezka w URL sie znajdzie
   - Ustawianie sciezek i metod http 
   - Routing to wykorzystanie sciezek i metod http do rozdzielenia zasobow
+  - Póki co jakiej ścieżki nie będziemy pisać w URL to dostaniemy tą samą odpowiedź co w res.end();
+  - WAŻNE: Ważne aby ustawić jaki mamy port
+
+  Jak działa Routing ?
+    - Praca na obiekcie request
+    - URL: req.url, Method: req.method
 
  */
 const http = require('http');
+//PORT
+const port = process.env.PORT || 3000;
+
 
 http.createServer((req, res) => {
-    res.end();
+    //How method and how url 
+    //console.log(req.url);
+    //console.log(req.method);
 
-}).listen(3000, '127.0.0.1');
+    //Czy nasz URL jest równy req.url
+    if (req.url === "/") {
+        res.writeHead(200, {
+            'Content-Type': 'text/html; charset=utf-8'
+        });
+        res.end(`<h1 style="color: green">MAIN PAGE! Your URL: ${req.url}</h1>`);
+    } else if (req.url === "/users") {
+        res.writeHead(200, {
+            'Content-Type': 'text/html; charset=utf-8'
+        });
+        res.end(`<h1 style="color: blue">USERS PAGE! Your URL: ${req.url}</h1>`)
+    } else {
+        //200 or 404
+        res.writeHead(404, {
+            'Content-Type': 'text/html; charset=utf-8'
+        });
+        res.end(`<h1 style="color: red"> NO PAGE! Your URL: ${req.url}</h1>`);
+    }
+
+
+    //res.end(`Hello server! Your url: ${req.url}`);
+
+}).listen(3000, '127.0.0.1', () => {
+    console.log(`PORT our servers is ... : ${port}`);
+});
