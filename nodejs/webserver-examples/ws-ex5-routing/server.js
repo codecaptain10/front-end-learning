1fd31f7931db63a5fca281c3d507af9182bd68bd
Szymon Szaminko <sz.szaminko@gmail.com>
2021-08-16 11:28:17 +0200

    Server with switch and creating two templates to load

--- nodejs/webserver-examples/ws-ex5-routing/server.js
+++ nodejs/webserver-examples/ws-ex5-routing/server.js
@@ -1,15 +1,39 @@
 //Example 5
 /* 
 Switch, pobranie zawartości plików dla konkretnych ścieżek i JSON
+
+    - Pobieranie plików i ich wyświetlanie (serwis internetowy)
+    - Pobieranie i wyświetlanie docelowo w Express.js
+    - Plan:
+        1) Instrukcja switch
+        2) Pobieranie i udostępnianie plików
+        3) Udostępnianie plików JSON
+        4) Pliki JavaScript
 */
 
 const http = require('http');
 const port = process.env.port || 3000;
 
 http.createServer((req, res) => {
+    //1) Switch 
+    switch (req.url) {
+        case '/':
+            res.end("<h1>Main page of our Website</h1>");
+            break;
+        case '/users':
+            res.end("<h1>Users of our Website</h1>");
+            break;
+        case '/api/users':
+            res.end("<h1>Users API</h1>");
+            break;
+        default:
+            res.end("<h1>Website doesn't exist!</h1>");
+
+    }
     res.writeHead(200, {
         'Content-Type': 'text/html; charset=utf-8'
     });
+
 }).listen(port, '127.0.0.1', () => {
     console.log(`Now Listening on ${port} ... `);
 });
